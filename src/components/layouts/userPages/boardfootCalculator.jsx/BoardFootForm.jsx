f837d654a6c4b3b5f7cae88f83bc8f07ca6f8655
JCWoodworker <jc@rilocalwoodworks.com>
2023-04-10 23:01:15 -0400

    full functionality of bf calc is available with tally saver.  tallies do not persist to a db yet and clear on refresh

--- src/components/layouts/userPages/boardfootCalculator.jsx/BoardFootForm.jsx
+++ src/components/layouts/userPages/boardfootCalculator.jsx/BoardFootForm.jsx
@@ -15,6 +15,7 @@ const BoardFootForm = ({ boardFootFormValues, setBoardFootFormValues }) => {
 					<label htmlFor="boardFootForm__length">Length</label>
 					<input
 						type="number"
+						min="0.01"
 						className="form-control"
 						id="boardFootForm_length"
 						placeholder="inches"
@@ -27,6 +28,7 @@ const BoardFootForm = ({ boardFootFormValues, setBoardFootFormValues }) => {
 					<label htmlFor="boardFootForm__width">Width</label>
 					<input
 						type="number"
+						min="0.01"
 						className="form-control"
 						id="boardFootForm_width"
 						placeholder="inches"
@@ -39,6 +41,7 @@ const BoardFootForm = ({ boardFootFormValues, setBoardFootFormValues }) => {
 					<label htmlFor="boardFootForm_thickness">Thickness</label>
 					<input
 						type="number"
+						min="0.01"
 						className="form-control"
 						id="boardFootForm_thickness"
 						placeholder="inches"
@@ -51,6 +54,7 @@ const BoardFootForm = ({ boardFootFormValues, setBoardFootFormValues }) => {
 					<label htmlFor="boardFootForm_price">Price</label>
 					<input
 						type="number"
+						min="0.01"
 						className="form-control"
 						id="boardFootForm_thickness"
 						placeholder="$/BF"
@@ -58,6 +62,29 @@ const BoardFootForm = ({ boardFootFormValues, setBoardFootFormValues }) => {
 						value={boardFootFormValues.price}
 						onChange={handleOnChange}
 					/>
+				</div>
+        {/* These options will eventually be mapped from a price-list table on the server */}
+				<div className="form-input-container">
+					<label htmlFor="woodType">Wood Type</label>
+					<select
+						name="woodType"
+						id="woodType"
+						onChange={handleOnChange}
+            defaultValue={'DEFAULT'}
+					>
+						<option value="DEFAULT" hidden disabled>
+							Choose...
+						</option>
+						<option value="Oak">Oak</option>
+						<option value="Maple">Maple</option>
+						<option value="Walnut">Walnut</option>
+						<option value="Cherry">Cherry</option>
+						<option value="Mahogany">Mahogany</option>
+						<option value="Cedar">Cedar</option>
+						<option value="Ash">Ash</option>
+						<option value="Poplar">Poplar</option>
+						<option value="Willow">Other</option>
+					</select>
 				</div>
 			</form>
 		</div>
--- src/components/layouts/userPages/boardfootCalculator.jsx/BoardFootIndex.jsx
+++ src/components/layouts/userPages/boardfootCalculator.jsx/BoardFootIndex.jsx
@@ -4,15 +4,57 @@ import BoardFootForm from "./BoardFootForm"
 
 const BoardFootIndex = () => {
 	const [boardFootFormValues, setBoardFootFormValues] = useState({
-		length: null,
-		width: null,
-		thickness: null,
-		price: null,
+		length: "",
+		width: "",
+		thickness: "",
+		price: "",
+		woodType: "",
 	})
 	const [boardFootResults, setBoardFootResults] = useState({
-		boardFeet: null,
-		cost: null,
+		boardFeet: "",
+		cost: "",
 	})
+	const [tallys, setTallys] = useState([])
+	const [tallyTotals, setTallyTotals] = useState({
+		totalBoardFeet: 0,
+		totalCost: 0,
+	})
+
+	const handleTallyAdd = () => {
+		if (
+			!boardFootResults.boardFeet ||
+			!boardFootResults.cost ||
+			parseInt(boardFootResults.boardFeet) <= 0 ||
+			parseInt(boardFootResults.cost) <= 0
+		) {
+			return alert("Please enter values for all fields")
+		}
+		return setTallys([
+			...tallys,
+			{
+				woodType: boardFootFormValues.woodType,
+				boardFeet: boardFootResults.boardFeet,
+				cost: boardFootResults.cost,
+			},
+		])
+	}
+
+	const handleTallyReset = () => {
+		setTallys([])
+	}
+
+	function handleDeleteSingleTally(event) {
+		event.preventDefault()
+		const confirmation = confirm("Are you sure you want to delete this tally?")
+		if (!confirmation) {
+			return
+		}
+		const clickedIndex = event.currentTarget.key;
+		const newTallys = [...tallys]
+		newTallys.splice(clickedIndex, 1)
+		setTallys(newTallys)
+
+	}
 
 	useEffect(() => {
 		const result = (
@@ -25,13 +67,48 @@ const BoardFootIndex = () => {
 		setBoardFootResults({ boardFeet: result, cost: cost })
 	}, [boardFootFormValues])
 
+	const sumBoardFeet = (arr) => {
+		let sum = 0
+		for (let i = 0; i < arr.length; i++) {
+			sum += parseFloat(arr[i].boardFeet)
+		}
+		return sum
+	}
+	const sumCost = (arr) => {
+		let sum = 0
+		for (let i = 0; i < arr.length; i++) {
+			sum += parseFloat(arr[i].cost)
+		}
+		return sum
+	}
+
+	useEffect(() => {
+		if (tallys.length === 0) {
+			setTallyTotals({ totalBoardFeet: 0, totalCost: 0 })
+		}
+		if (tallys.length >= 1) {
+			const totalBoardFeet = sumBoardFeet(tallys).toFixed(2)
+			const totalCost = sumCost(tallys).toFixed(2)
+			setTallyTotals({ totalBoardFeet: totalBoardFeet, totalCost: totalCost })
+		}
+	}, [tallys])
+
+	let tallysList = null
+	if (tallys.length > 0) {
+		tallysList = tallys.map((tally, index) => {
+			return (
+				<li key={index} onClick={handleDeleteSingleTally}>
+					{tally.woodType} - ${tally.cost} - {tally.boardFeet} BF
+					<span className="delete-tally">X</span>
+				</li>
+			)
+		})
+	}
+
 	return (
 		<div>
 			<div>
-				<h1>
-					BF Calc<i className="bi bi-cone-striped"></i>
-				</h1>
-				<p>Under Construction</p>
+				<h1>Board Foot Calculator</h1>
 			</div>
 			<div className="bfc-form-and-results-container">
 				<div className="bfc-form-container">
@@ -43,11 +120,21 @@ const BoardFootIndex = () => {
 				<div className="bfc-results-container">
 					<h3>Results</h3>
 					<p>Board Feet: {boardFootResults.boardFeet}</p>
-          <p>Cost: ${boardFootResults.cost}</p>
+					<p>Cost: ${boardFootResults.cost}</p>
+					<div>
+						<button onClick={handleTallyAdd}>Add Tally</button>
+						<button onClick={handleTallyReset}>Reset Tallies</button>
+					</div>
 				</div>
 			</div>
 			<div className="bfc-tally-container">
-				<p>Tallys</p>
+				<h3>
+					TALLYS:{" "}
+					<span>
+						{tallys.length > 0 ? `${tallyTotals.totalBoardFeet} board feet - $${tallyTotals.totalCost}` : `None yet`}
+					</span>
+				</h3>
+				<ul>{tallysList}</ul>
 			</div>
 		</div>
 	)
