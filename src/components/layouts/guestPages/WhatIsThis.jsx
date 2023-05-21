9c0c5c161912588f349c52e60bf3eddaf9572ffd
JCWoodworker <jc@rilocalwoodworks.com>
2023-05-21 02:38:49 -0400

    changed styling of containers to fill screen more

--- src/components/layouts/guestPages/WhatIsThis.jsx
+++ src/components/layouts/guestPages/WhatIsThis.jsx
@@ -10,7 +10,8 @@ const WhatIsThis = () => {
 			<Container className="what-is-this">
 				<Row className="d-flex justify-content-center align-items-center text-center">
 					<h3>
-						This is a project management tool <br /> for small woodworking businesses.
+						This is a project management tool <br /> for small woodworking
+						businesses.
 					</h3>
 					{/* <Image
 						fluid
@@ -20,14 +21,14 @@ const WhatIsThis = () => {
 					/> */}
 				</Row>
 				<Row className="d-flex justify-content-center align-items-center text-center">
-						<h3>
-							Use it to calculate costs, estimate retail pricing, <br /> and even
-							manage customers with a built-in CRM.
-						</h3>
+					<h3>
+						Use it to calculate costs, estimate retail pricing, <br /> and even
+						manage customers with a built-in CRM.
+					</h3>
 				</Row>
 				<Row className="d-flex justify-content-center align-items-center text-center row-bottom-text-container">
-						<h5>It's free to use, give it a try!</h5>
-						<h5>⬇️ Use the sign up form below ⬇️</h5>
+					<h5>It's free to use, give it a try!</h5>
+					<h5>⬇️ Use the sign up form below ⬇️</h5>
 				</Row>
 			</Container>
 		</>
