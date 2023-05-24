8eb965ae10ae910deea7fe128bb042dac58ab98f
JCWoodworker <jc@rilocalwoodworks.com>
2023-05-24 16:32:28 -0400

    set up analyticts component

---
+++ src/components/layouts/userPages/analytics/UserAnalytics.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const UserAnalytics = () => {
+	return (
+		<>
+			<h1>
+				Analytics
+				<i className="bi bi-cone-striped"></i>
+			</h1>
+			<p>Under Construction</p>
+		</>
+	)
+}
+
+export default UserAnalytics
